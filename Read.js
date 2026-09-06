d7800986b8c684d4630f4e7d041020daa0132ee0
Learning <sksaravana235@gmail.com>
2026-09-06 08:28:40 +0530

    Giri and Thanya printing

--- Read.js
+++ Read.js
@@ -1,2 +1,6 @@
 console('Hello world!, New start new beginings')
 console('Current data is currently being processed') 
+
+for(var i=0;i<5;i++){
+    console.log('Girivasan and Thanya and making conflict for main branch')
+}
