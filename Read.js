25906ad95154257bb147ac1e7383200ebcf1cef6
Learning <sksaravana235@gmail.com>
2026-09-06 08:21:32 +0530

    print the Sanjeevakumar Arjunan in main branch

--- Read.js
+++ Read.js
@@ -1,2 +1,6 @@
 console('Hello world!, New start new beginings')
 console('Current data is currently being processed') 
+console('Lets play fun play game')
+for(var i=0;i<5;i++){
+    console.log("Sanjeevakumar Arjunan");
+}
